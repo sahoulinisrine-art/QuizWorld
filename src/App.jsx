ee70b8c5e9efc990ad5e873d673df6c53e04ff68
NisRin <sahoulinisrine@gmail.com>
2026-05-06 20:15:19 +0200

    React Router nav pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Quiz from "./pages/Quiz";
+import Results from "./pages/Results";
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/quiz" element={<Quiz />} />
+        <Route path="/results" element={<Results />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
